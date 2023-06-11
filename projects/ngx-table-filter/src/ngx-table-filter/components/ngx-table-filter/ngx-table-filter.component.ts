321664271eda4223c284eed625a546b08ac26fe6
Arges <wolf.arges@gmail.com>
2023-06-11 15:45:02 +0200

    SEG - Add cookieModule init filters

--- projects/ngx-table-filter/src/ngx-table-filter/components/ngx-table-filter/ngx-table-filter.component.ts
+++ projects/ngx-table-filter/src/ngx-table-filter/components/ngx-table-filter/ngx-table-filter.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { INgxItemFilter, INgxTableOptions } from '../../interfaces/config.interface';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'ngx-table-filter',
@@ -10,18 +11,23 @@ import { INgxItemFilter, INgxTableOptions } from '../../interfaces/config.interf
 export class NgxTableFilterComponent implements OnInit {
   public itemsFilters: { [key: string]: INgxItemFilter[] } = {};
   public data: any[] = [];
-  private applyFilters: { [key: string]: string[] | number[] } = {};
+  public applyFilters: { [key: string]: string[] | number[] } = {};
   private fileName: string = "csv_";
 
   @Input() items: any[] = [];
   @Input() options: INgxTableOptions = [];
   @Input() exportCSV: boolean = true;
+  @Input() cookies: string = "ngx-table-filter";
 
-  constructor(private datePipe: DatePipe) {}
+  constructor(
+    private datePipe: DatePipe,
+    private cookieService: CookieService) { }
 
   public ngOnInit(): void {
     this.data = this.items;
+
     this.ngOnInitFilter();
+    this.ngOnInitCookies();
   }
 
   public onChangeFilter(key: string, filter: INgxItemFilter[] | INgxItemFilter): void {
@@ -32,13 +38,14 @@ export class NgxTableFilterComponent implements OnInit {
     }
 
     this.ngOnFilter();
+    this.cookieService.set(this.cookies, JSON.stringify(this.applyFilters));
   }
 
   public exportCsv(): void {
     let csv = '';
 
     /* Header CSV */
-    for(let col in this.options) {
+    for (let col in this.options) {
       csv += this.options[col].label + ';';
       csv = csv.replace(/\n/g, '');
     }
@@ -50,7 +57,7 @@ export class NgxTableFilterComponent implements OnInit {
 
     /* Body CSV */
     for (let row = 0; row < rows.length; row++) {
-      for(let col in rows[row]){
+      for (let col in rows[row]) {
         csv += rows[row][col] + ';';
       }
       csv = csv.substring(0, csv.length - 1) + '\n';
@@ -60,15 +67,23 @@ export class NgxTableFilterComponent implements OnInit {
     csv = csv.substring(0, csv.length - 1) + '\n';
     const docElement = document.createElement('a');
     const universalBOM = '\uFEFF';
-  
+
     var date = new Date();
     let filename = this.fileName;
-    filename = filename.concat(this.datePipe.transform(date,"yyyy-MM-dd")!.toString());
+    filename = filename.concat(this.datePipe.transform(date, "yyyy-MM-dd")!.toString());
     const fileNameWithType = filename.concat('.csv');
     docElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(universalBOM + csv);
     docElement.target = '_blank';
     docElement.download = fileNameWithType;
     docElement.click();
+  }
+
+  private ngOnInitCookies(): void {
+    if(this.cookieService.check(this.cookies)) {
+      this.applyFilters = JSON.parse(this.cookieService.get(this.cookies));
+
+      this.ngOnFilter();
+    }
   }
 
   private ngOnInitFilter(): void {
@@ -79,13 +94,13 @@ export class NgxTableFilterComponent implements OnInit {
         this.applyFilters[key] = [];
 
         /* this.itemsFilters[key] = */
-          const arrDuplicates = this.items.map((item: any) => {
-            return <INgxItemFilter>{ value: item[key], label: item[key] }
-          });
-        
-          const arrNotDuplicates = <INgxItemFilter[]>this.getUniqueListBy(arrDuplicates);
+        const arrDuplicates = this.items.map((item: any) => {
+          return <INgxItemFilter>{ value: item[key], label: item[key] }
+        });
 
-          this.itemsFilters[key] = arrNotDuplicates;
+        const arrNotDuplicates = <INgxItemFilter[]>this.getUniqueListBy(arrDuplicates);
+
+        this.itemsFilters[key] = arrNotDuplicates;
       }
     }
   }
@@ -95,7 +110,7 @@ export class NgxTableFilterComponent implements OnInit {
     this.data = this.items;
 
     for (let filter in this.applyFilters) {
-      if (this.applyFilters[filter].length > 0 ) {
+      if (this.applyFilters[filter].length > 0) {
         this.data = arrDataFilter.filter((item: any) => {
           const PATTERN = item[filter];
           return this.applyFilters[filter].indexOf(<never>PATTERN) !== -1 ? true : false;
@@ -110,7 +125,7 @@ export class NgxTableFilterComponent implements OnInit {
   private getUniqueListBy(arr: any) {
     return arr.filter(
       (item: INgxItemFilter, index: number) => index === arr.findIndex(
-        (find:INgxItemFilter) => item.value === find.value
+        (find: INgxItemFilter) => item.value === find.value
           && item.value === find.value
       ));
   }
--- projects/ngx-table-filter/src/ngx-table-filter/ngx-table-filter.module.ts
+++ projects/ngx-table-filter/src/ngx-table-filter/ngx-table-filter.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgxTableFilterComponent } from './components/ngx-table-filter/ngx-table-filter.component';
 
 import { NgSelectModule } from '@ng-select/ng-select';
 
 import { DatePipe } from '@angular/common';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [
@@ -12,11 +14,12 @@ import { DatePipe } from '@angular/common';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     NgSelectModule
   ],
   exports: [
     NgxTableFilterComponent
   ],
-  providers: [DatePipe]
+  providers: [DatePipe, CookieService]
 })
 export class NgxTableFilterModule { }
