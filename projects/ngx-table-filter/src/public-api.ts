5bc719ced69c32326014b2b33e6da82681648ad7
Arges <wolf.arges@gmail.com>
2023-06-13 12:28:10 +0200

    SEG - Fix api export component

--- projects/ngx-table-filter/src/public-api.ts
+++ projects/ngx-table-filter/src/public-api.ts
@@ -7,4 +7,5 @@ export * from './lib/ngx-table-filter.component';
 export * from './lib/ngx-table-filter.module';
 
 export { NgxTableFilterModule } from "./ngx-table-filter/ngx-table-filter.module";
+export { NgxTableFilterComponent } from "./ngx-table-filter/components/ngx-table-filter/ngx-table-filter.component";
 export { INgxTableConfig, INgxTableOptions } from "./ngx-table-filter/interfaces/config.interface";
